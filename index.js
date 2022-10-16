fb6e0880a874e599ac916a2c9b3bf9ec5efdebe0
nickson <nowembabazin8@gmail.com>
2022-10-16 01:07:09 -0500

    linter errors corrected

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-import navSection from "./modules/navSection.js";
-import displayBook from "./modules/displaybooks.js";
-import allBooks from "./modules/awesomebooks.js";
-import { DateTime } from "./modules/luxon.js";
+import navSection from './modules/navSection.js';
+import displayBook from './modules/displaybooks.js';
+import allBooks from './modules/awesomebooks.js';
+import { DateTime } from './modules/luxon.js';
 
 import {
   addToLocalStorage,
   getFromLocalStorage,
-} from "./modules/localstorage.js";
+} from './modules/localstorage.js';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   navSection();
   displayBook();
 });
 
 // add-book
-const addBtn = document.getElementById("addButton");
-addBtn.addEventListener("click", () => {
-  const title = document.getElementById("title");
-  const author = document.getElementById("author");
+const addBtn = document.getElementById('addButton');
+addBtn.addEventListener('click', () => {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
   allBooks.bookObj(title.value, author.value);
   addToLocalStorage();
 });
 
-if (localStorage.getItem("storedBooks") == null) {
+if (localStorage.getItem('storedBooks') == null) {
   addToLocalStorage();
-} else{
+} else {
   getFromLocalStorage();
 }
 
 const localDatetime = DateTime.local().toLocaleString(
-  DateTime.DATETIME_FULL_WITH_SECONDS
+  DateTime.DATETIME_FULL_WITH_SECONDS,
 );
 
-const currentDate = document.querySelector(".date");
+const currentDate = document.querySelector('.date');
 currentDate.textContent = localDatetime;
