e9a14061481be5d601af012c0ce66ce965cd9273
nickson <nowembabazin8@gmail.com>
2022-10-16 00:33:46 -0500

    Add changes

--- modules/displaybooks.js
+++ modules/displaybooks.js
@@ -6,22 +6,22 @@ const displayBook = () => {
   addedBooks.innerHTML = '';
   if (allBooks.bookArray.length) {
     for (let i = 0; i < allBooks.bookArray.length; i += 1) {
-      const container2 = document.createElement('div');
-      container2.classList.add('titleAuthor');
-      container.appendChild(container2);
-
       const container = document.createElement('div');
       container.classList.add('book');
       addedBooks.appendChild(container);
 
-      const removeButton = document.createElement('button');
-      removeButton.classList.add('remove');
-      removeButton.textContent = 'Remove';
+      const container2 = document.createElement('div');
+      container2.classList.add('titleAuthor');
+      container.appendChild(container2);
 
       const bookDetails = document.createElement('p');
       bookDetails.classList.add('title');
       bookDetails.textContent = `"${allBooks.bookArray[i].title}" by ${allBooks.bookArray[i].author}`;
       container2.appendChild(bookDetails);
+
+      const removeButton = document.createElement('button');
+      removeButton.classList.add('remove');
+      removeButton.textContent = 'Remove';
 
       removeButton.onclick = () => {
         allBooks.booksFilter(allBooks.bookArray[i]);
